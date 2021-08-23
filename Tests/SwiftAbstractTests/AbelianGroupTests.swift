5a87b41b60cf590be89e03cdd22cc52a14be619b
Elviro Rocca <elviro.rocca@icloud.com>
2021-08-23 10:02:37 +0200

    Better testing

--- Tests/SwiftAbstractTests/AbelianGroupTests.swift
+++ Tests/SwiftAbstractTests/AbelianGroupTests.swift
@@ -1,45 +1,44 @@
 @testable import SwiftAbstract
 import SwiftCheck
 import XCTest
+//import TSCUtility
 
 final class AbelianGroupTests: XCTestCase {
-  func testProperties() {}
-
-  func testFunctionInstancesProperties() {
-    struct GeneratedStructure: Arbitrary {
-      let get: AbelianGroup<Int>
-
-      static var arbitrary: Gen<GeneratedStructure> {
-        Gen<AbelianGroup<Int>>.fromElements(of: [
-          .sum
-        ]).map {
-          GeneratedStructure(get: $0)
-        }
-      }
-    }
-
-//    property("AbelianGroup.function respects some laws") <- forAll {
-//      (
-//        a: ArrowOf<String, Int>,
-//        b: ArrowOf<String, Int>,
-//        c: ArrowOf<String, Int>,
-//        structure: GeneratedStructure,
-//        value: String
-//      ) in
-//      AbelianGroup<(String) -> Int>.function(over: structure.get)
-//        .getProperties(equating: { $0(value) == $1(value) })
-//        .map { property in
-//          TestResult(
-//            require: "AbelianGroup.function \(property.name)",
-//            check: property.verify(a.getArrow, b.getArrow, c.getArrow)
-//          )
-//        }
-//        .reduce(TestResult.succeeded.property) { conjoin($0, $1) }
-//    }
+  func testProperties() {
+      _verifyAllProperties(
+        ofFunctionBasedStructure: AbelianGroup<(String) -> Int>.self,
+        onInstances: [("sum", AbelianGroup<Int>.sum)],
+        constructedBy: { .function(over: $0) },
+        equating: { input in { $0(input) == $1(input) } }
+      )
   }
 
   static var allTests = [
     ("testProperties", testProperties),
-//    ("testFunctionInstancesProperties", testFunctionInstancesProperties)
   ]
+}
+
+func _verifyAllProperties<Structure: AlgebraicStructure, OutputStructure: AlgebraicStructure, Input>(
+  ofFunctionBasedStructure algebraicStructure: Structure.Type,
+  onInstances instances: [(instance: String, value: OutputStructure)],
+  constructedBy getStructure: @escaping (OutputStructure) -> Structure,
+  equating: @escaping (Input) -> (Structure.A, Structure.A) -> Bool,
+  file: StaticString = #file,
+  line: UInt = #line
+) where Structure.A == (Input) -> OutputStructure.A, Input: Hashable & CoArbitrary & Arbitrary, OutputStructure.A: Arbitrary {
+    property("\(algebraicStructure) instances respect some laws", file: file, line: line) <- forAll { (a: ArrowOf<Input,  OutputStructure.A>, b: ArrowOf<Input,  OutputStructure.A>, c: ArrowOf<Input,  OutputStructure.A>, value: Input) in
+    instances
+      .flatMap { name, instance in
+          Structure._properties.map {
+              (name, _Verify(structure: getStructure(instance), equating: equating(value), property: $0))
+          }
+      }
+      .map { name, verify in
+        TestResult(
+            require: "\(algebraicStructure).\(name) \(verify.property.name)",
+            check: verify(a.getArrow, b.getArrow, c.getArrow)
+        )
+      }
+      .reduce(TestResult.succeeded.property) { conjoin($0, $1) }
+  }
 }
