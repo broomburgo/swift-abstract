adc7e419c72196000e5653000fad0b6f8ff6be00
Elviro Rocca <elviro.rocca@icloud.com>
2021-09-04 17:59:44 +0200

    Better AlgebraicInstance

--- Sources/SwiftAbstract/Foundation/AlgebraicInstance.swift
+++ Sources/SwiftAbstract/Foundation/AlgebraicInstance.swift
@@ -1,56 +1,31 @@
+protocol AlgebraicInstance {
+  associatedtype ReferenceStructure: AlgebraicStructure where ReferenceStructure.A == Self
+  static var referenceInstance: ReferenceStructure { get }
+}
+
 protocol Wrapper {
   associatedtype Wrapped
   init(_ wrapped: Wrapped)
   var wrapped: Wrapped { get }
 }
 
-extension Wrapper where Wrapped == Self {
-  init(_ wrapped: Wrapped) {
-    self = wrapped
-  }
-
-  var wrapped: Wrapped {
-    self
-  }
-}
-
-protocol AlgebraicInstance: Wrapper {
-  associatedtype ReferenceStructure: AlgebraicStructure where ReferenceStructure.A == Wrapped
-  static var referenceInstance: ReferenceStructure { get }
-}
-
-extension String: AlgebraicInstance {
-  typealias Wrapped = Self
-  static let referenceInstance = Monoid.string
-}
-
-extension Array: AlgebraicInstance {
-  typealias Wrapped = Self
-  static var referenceInstance: Monoid<Self> {
-    .array()
-  }
+func wrapping<_Wrapper>(_ original: Monoid<_Wrapper.Wrapped>) -> Monoid<_Wrapper> where _Wrapper: Wrapper {
+  .init(
+    apply: {
+      .init(original.apply($0.wrapped, $1.wrapped))
+    },
+    empty: .init(original.empty)
+  )
 }
 
 struct Sum<Wrapped>: Wrapper where Wrapped: AdditiveArithmetic {
-  let wrapped: Wrapped
+  var wrapped: Wrapped
+
   init(_ wrapped: Wrapped) {
     self.wrapped = wrapped
   }
 }
 
 extension Sum: AlgebraicInstance {
-  static var referenceInstance: Monoid<Wrapped> {
-    .addition
-  }
-}
-
-protocol MonoidInstance: AlgebraicInstance where ReferenceStructure == Monoid<Wrapped> {}
-
-extension Sequence where Element: MonoidInstance {
-  func concat() -> Element {
-    let instance = Element.referenceInstance
-    return reduce(.init(instance.empty)) {
-      .init(instance.apply($0.wrapped, $1.wrapped))
-    }
-  }
+  static var referenceInstance: Monoid<Self> { wrapping(.addition) }
 }
