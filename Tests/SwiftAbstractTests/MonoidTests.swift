392a8b7185567362d70f4948a1609dc8fd26119f
Elviro Rocca <elviro.rocca@icloud.com>
2021-09-15 08:25:41 +0200

    Add MonoidTests

---
+++ Tests/SwiftAbstractTests/MonoidTests.swift
@@ -0,0 +1,57 @@
+@testable import SwiftAbstract
+import SwiftCheck
+import XCTest
+
+final class MonoidTests: XCTestCase {
+  func testLawsOfRegularStructure() {
+    verifyAllLaws(
+      ofStructure: Monoid<String>.self,
+      onInstances: [
+        ("string", .string),
+      ],
+      equating: ==
+    )
+
+    verifyAllLaws(
+      ofStructure: Monoid<[Int]>.self,
+      onInstances: [
+        ("array", .array()),
+      ],
+      equating: ==
+    )
+
+    verifyAllLaws(
+      ofStructure: Monoid<(Int) -> Int>.self,
+      onInstances: [
+        ("", .endo())
+      ],
+      equating: { (input: Int) in { $0(input) == $1(input) } },
+      generating: { (generated: ArrowOf<Int, Int>) in generated.getArrow }
+    )
+  }
+
+  func testLawsOfFunctionBasedStructure() {
+    verifyAllLaws(
+      ofFunctionBasedStructure: Monoid<(String) -> String>.self,
+      onInstances: [
+        ("string", .string),
+      ],
+      constructedBy: { .function(over: $0) },
+      equating: { input in { $0(input) == $1(input) } }
+    )
+
+    verifyAllLaws(
+      ofFunctionBasedStructure: Monoid<(String) -> [Int]>.self,
+      onInstances: [
+        ("array", .array()),
+      ],
+      constructedBy: { .function(over: $0) },
+      equating: { input in { $0(input) == $1(input) } }
+    )
+  }
+
+  static var allTests = [
+    ("testLawsOfRegularStructure", testLawsOfRegularStructure),
+    ("testLawsOfFunctionBasedStructure", testLawsOfFunctionBasedStructure),
+  ]
+}
--- Tests/SwiftAbstractTests/TestHelpers.swift
+++ Tests/SwiftAbstractTests/TestHelpers.swift
@@ -2,115 +2,141 @@
 import SwiftCheck
 
 struct Verify<Structure: AlgebraicStructure> {
-    let law: Law<Structure>
-    let structure: Structure
-    let equating: (Structure.A, Structure.A) -> Bool
+  let law: Law<Structure>
+  let structure: Structure
+  let equating: (Structure.A, Structure.A) -> Bool
 
-    func callAsFunction(_ a: Structure.A, _ b: Structure.A, _ c: Structure.A) -> Bool {
-        switch law.getCheck(structure, equating) {
-        case let .fromOne(f):
-            return f(a)
+  func callAsFunction(_ a: Structure.A, _ b: Structure.A, _ c: Structure.A) -> Bool {
+    switch law.getCheck(structure, equating) {
+    case .fromOne(let f):
+      return f(a)
 
-        case let .fromTwo(f):
-            return f(a, b)
+    case .fromTwo(let f):
+      return f(a, b)
 
-        case let .fromThree(f):
-            return f(a, b, c)
-        }
+    case .fromThree(let f):
+      return f(a, b, c)
     }
+  }
 }
 
 extension Ordering: Arbitrary {
-    public static var arbitrary: Gen<Ordering> {
-        Gen.fromElements(of: Ordering.allCases)
-    }
+  public static var arbitrary: Gen<Ordering> {
+    Gen.fromElements(of: Ordering.allCases)
+  }
 }
 
 extension TestResult {
-    init(require: String, check: Bool) {
-        self = check ? .succeeded : .failed(require)
-    }
+  init(require: String, check: Bool) {
+    self = check ? .succeeded : .failed(require)
+  }
 }
 
 func verifyAllLaws<Structure: AlgebraicStructure>(
-    ofStructure algebraicStructure: Structure.Type,
-    onInstances instances: [(instance: String, value: Structure)],
-    equating: @escaping (Structure.A, Structure.A) -> Bool,
-    arguments : CheckerArguments? = nil,
-    file: StaticString = #file,
-    line: UInt = #line
+  ofStructure algebraicStructure: Structure.Type,
+  onInstances instances: [(instance: String, value: Structure)],
+  equating: @escaping (Structure.A, Structure.A) -> Bool,
+  arguments: CheckerArguments? = nil,
+  file: StaticString = #file,
+  line: UInt = #line
 ) where Structure.A: Arbitrary {
-    property("\(algebraicStructure) instances respect some laws", arguments: arguments, file: file, line: line) <- forAll { (a: Structure.A, b: Structure.A, c: Structure.A) in
-        instances
-            .flatMap { instance, value in
-                Structure.laws.map {
-                    (instance, Verify(law: $0, structure: value, equating: equating))
-                }
-            }
-            .map { instance, verify in
-                TestResult(
-                    require: "\(algebraicStructure).\(instance) \(verify.law.name)",
-                    check: verify(a, b, c)
-                )
-            }
-            .reduce(TestResult.succeeded.property) { conjoin($0, $1) }
-    }
+  property("\(algebraicStructure) instances respect some laws", arguments: arguments, file: file, line: line) <- forAll { (a: Structure.A, b: Structure.A, c: Structure.A) in
+    instances
+      .flatMap { instance, value in
+        Structure.laws.map {
+          (instance, Verify(law: $0, structure: value, equating: equating))
+        }
+      }
+      .map { instance, verify in
+        TestResult(
+          require: "\(algebraicStructure).\(instance) \(verify.law.name)",
+          check: verify(a, b, c)
+        )
+      }
+      .reduce(TestResult.succeeded.property) { conjoin($0, $1) }
+  }
+}
+
+func verifyAllLaws<Structure: AlgebraicStructure, Generated: Arbitrary, Extra: Arbitrary>(
+  ofStructure algebraicStructure: Structure.Type,
+  onInstances instances: [(instance: String, value: Structure)],
+  equating: @escaping (Extra) -> (Structure.A, Structure.A) -> Bool,
+  generating: @escaping (Generated) -> Structure.A,
+  arguments: CheckerArguments? = nil,
+  file: StaticString = #file,
+  line: UInt = #line
+) {
+  property("\(algebraicStructure) instances respect some laws", arguments: arguments, file: file, line: line) <- forAll { (ag: Generated, bg: Generated, cg: Generated, extra: Extra) in
+    instances
+      .flatMap { instance, value in
+        Structure.laws.map {
+          (instance, Verify(law: $0, structure: value, equating: equating(extra)))
+        }
+      }
+      .map { instance, verify in
+        TestResult(
+          require: "\(algebraicStructure).\(instance) \(verify.law.name)",
+          check: verify(generating(ag), generating(bg), generating(cg))
+        )
+      }
+      .reduce(TestResult.succeeded.property) { conjoin($0, $1) }
+  }
 }
 
 func verifyAllLaws<Structure: AlgebraicStructure, OutputStructure: AlgebraicStructure, Input>(
-    ofFunctionBasedStructure algebraicStructure: Structure.Type,
-    onInstances instances: [(instance: String, value: OutputStructure)],
-    constructedBy getStructure: @escaping (OutputStructure) -> Structure,
-    equating: @escaping (Input) -> (Structure.A, Structure.A) -> Bool,
-    file: StaticString = #file,
-    line: UInt = #line
+  ofFunctionBasedStructure algebraicStructure: Structure.Type,
+  onInstances instances: [(instance: String, value: OutputStructure)],
+  constructedBy getStructure: @escaping (OutputStructure) -> Structure,
+  equating: @escaping (Input) -> (Structure.A, Structure.A) -> Bool,
+  file: StaticString = #file,
+  line: UInt = #line
 ) where Structure.A == (Input) -> OutputStructure.A, Input: Hashable & CoArbitrary & Arbitrary, OutputStructure.A: Arbitrary {
-    property("\(algebraicStructure) instances respect some laws", file: file, line: line) <- forAll { (a: ArrowOf<Input, OutputStructure.A>, b: ArrowOf<Input, OutputStructure.A>, c: ArrowOf<Input, OutputStructure.A>, value: Input) in
-        instances
-            .flatMap { name, instance in
-                Structure.laws.map {
-                    (name, Verify(law: $0, structure: getStructure(instance), equating: equating(value)))
-                }
-            }
-            .map { name, verify in
-                TestResult(
-                    require: "\(algebraicStructure).\(name) \(verify.law.name)",
-                    check: verify(a.getArrow, b.getArrow, c.getArrow)
-                )
-            }
-            .reduce(TestResult.succeeded.property) { conjoin($0, $1) }
-    }
+  property("\(algebraicStructure) instances respect some laws", file: file, line: line) <- forAll { (a: ArrowOf<Input, OutputStructure.A>, b: ArrowOf<Input, OutputStructure.A>, c: ArrowOf<Input, OutputStructure.A>, value: Input) in
+    instances
+      .flatMap { name, instance in
+        Structure.laws.map {
+          (name, Verify(law: $0, structure: getStructure(instance), equating: equating(value)))
+        }
+      }
+      .map { name, verify in
+        TestResult(
+          require: "\(algebraicStructure).\(name) \(verify.law.name)",
+          check: verify(a.getArrow, b.getArrow, c.getArrow)
+        )
+      }
+      .reduce(TestResult.succeeded.property) { conjoin($0, $1) }
+  }
 }
 
 extension CheckerArguments {
-    static func seedDescription(_ replayString: String) -> CheckerArguments {
-        /// Expected format: "Replay with 711250669 9042 and size 7"
-        let arguments = replayString
-            .split(separator: " ")
-            .compactMap { Int($0) }
+  static func seedDescription(_ replayString: String) -> CheckerArguments {
+    /// Expected format: "Replay with 711250669 9042 and size 7"
+    let arguments = replayString
+      .split(separator: " ")
+      .compactMap { Int($0) }
 
-        return CheckerArguments(replay: (StdGen(arguments[0], arguments[1]), arguments[2]))
-    }
+    return CheckerArguments(replay: (StdGen(arguments[0], arguments[1]), arguments[2]))
+  }
 }
 
 extension FloatingPoint where Self.Stride: ExpressibleByFloatLiteral {
-    func isAlmostEqual(to other: Self, tolerance: Self.Stride = 0.0001) -> Bool {
-        if self == .infinity, other == .infinity {
-            return true
-        }
-
-        if self == -.infinity, other == -.infinity {
-            return true
-        }
-
-        guard self != 0 else {
-            return abs(other.distance(to: 0)) < tolerance
-        }
-
-        guard other != 0 else {
-            return abs(distance(to: 0)) < tolerance
-        }
-
-        return abs((self / other).distance(to: 1)) < tolerance
+  func isAlmostEqual(to other: Self, tolerance: Self.Stride = 0.0001) -> Bool {
+    if self == .infinity, other == .infinity {
+      return true
     }
+
+    if self == -.infinity, other == -.infinity {
+      return true
+    }
+
+    guard self != 0 else {
+      return abs(other.distance(to: 0)) < tolerance
+    }
+
+    guard other != 0 else {
+      return abs(distance(to: 0)) < tolerance
+    }
+
+    return abs((self / other).distance(to: 1)) < tolerance
+  }
 }
